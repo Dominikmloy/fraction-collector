514ea7aefa56fd5c718556e10b14aaca2ded31b4
Dominik <dominikmloy@gmail.com>
2019-11-06 16:59:25 +0100

    Initialize.py: module is ready now. main.py: reaching starting position of stepper one accomplished.

--- initialize.py
+++ initialize.py
@@ -1,8 +1,5 @@
 # raspberry
 # !/usr/bin/python
-import os
-import sys
-from time import *
 import RPi.GPIO as GPIO
 
 
@@ -10,9 +7,18 @@ class Initialize(object):
     """This class initializes the fraction collector and resets it to position
      0/0 on the x/y grids coordinate system. """
     def __init__(self):
+        # callable variables used in this Method
+        self.out_pins = []
+        self.in_pins = []
+        self.mask_dl = []
+        self.mask_dr = []
+        # speeds: 0.3 sec is very slow -> no stepping errors
+        # 0.002 sec is possible without errors
+        self.speeds = {"s100": 0.005, "s75": 0.01, "s50": 0.025, "s25": 0.05, "s0": 0.1}
         # map pins to a stepper and end switch
         self.pins_stepper1 = {"A": 18, "B": 23, "C": 24, "D": 25, "stop_1": 27}
         self.pins_stepper2 = {"A": 5, "B": 6, "C": 13, "D": 26, "stop_2": 17}
+
         # define stepper pins as outputs and
         # set the output of the stepper pins to low (achieved using "False")
         GPIO.setmode(GPIO.BCM)
@@ -25,13 +31,15 @@ class Initialize(object):
             if item in "ABCD":
                 GPIO.setup(self.pins_stepper2[item], GPIO.OUT)  # defines as output
                 GPIO.setup(self.pins_stepper2[item], False)     # set to low
-        # define end switch pins as
+
+        # define end switch pins as inputs
         GPIO.setup(self.pins_stepper1["stop_1"], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.pins_stepper2["stop_2"], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
         # gather in and out pins in two variables
-        self.out_pins = []
-        self.in_pins = []
-        for value in self.pins_stepper1.values():
+        for key in sorted(self.pins_stepper1.keys()):  # included the 'sorted' function to make sure that
+            # out_pins is in the correct order
+            value = self.pins_stepper1[key]
             gpio_function = GPIO.gpio_function(value)
             if gpio_function == 1:
                 self.in_pins.append(value)
@@ -39,7 +47,9 @@ class Initialize(object):
                 self.out_pins.append(value)
             else:
                 print("Pin with number {} is not defined.".format(value))
-        for value in self.pins_stepper2.values():
+
+        for key in sorted(self.pins_stepper2.keys()):
+            value = self.pins_stepper2[key]
             gpio_function = GPIO.gpio_function(value)
             if gpio_function == 1:
                 self.in_pins.append(value)
@@ -47,5 +57,44 @@ class Initialize(object):
                 self.out_pins.append(value)
             else:
                 print("Pin with number {} is not defined.".format(value))
+
         print("out pins: {}, in pins: {}".format(self.out_pins, self.in_pins))
+
+        # define pattern for each step of the steppers
+        patterns = [[1, 0, 1, 0], [1, 0, 0, 1], [0, 1, 0, 1], [0, 1, 1, 0]]
+        # turn stepper in direction "left"
+        self.mask_dl = patterns[0:4]
+        # turn stepper in directin "right"
+        self.mask_dr = list(reversed(patterns[0:4]))
+        # add 'global' statement to access stop_xA before function _stop_move was called.
+        global stop_xA, stop_yB
+        stop_xA = 0
+        stop_yB = 0
+
+        def _stop_move_xA(stop):
+            global stop_xA
+            if GPIO.input(stop) == 0:  # end switch pressed
+                stop_xA = 1
+                print("End switch A pressed.")
+            else:  # end switch released or not pressed
+                stop_xA = 0
+                print("End switch A released.")
+
+        def _stop_move_yB(stop):
+            global stop_yB
+            if GPIO.input(stop) == 0:  # end switch pressed
+                stop_yB = 1
+                print("End switch B pressed.")
+            else:  # end switch released or not pressed
+                stop_yB = 0
+                print("End switch B released.")
+
+        GPIO.add_event_detect(self.pins_stepper1["stop_1"],
+                              GPIO.BOTH,
+                              callback=_stop_move_xA,
+                              bouncetime=200)
+        GPIO.add_event_detect(self.pins_stepper2["stop_2"],
+                              GPIO.BOTH,
+                              callback=_stop_move_yB,
+                              bouncetime=200)
 
--- main.py
+++ main.py
@@ -2,176 +2,57 @@
 #!/usr/bin/python
 import os
 import sys
-from time import *
 import RPi.GPIO as GPIO
+from time import *
+import initialize as ini
 # import threading # this module is going to be used in future versions to enable the simultaneous addressing
 # of pumps and steppers.
 
-main_start = True
-main_stop = False
-##########################################################
-# Used pins from the raspberry pi GPIO interface
-A = 18
-B = 23
-C = 24
-D = 25
-# alternative code for pins:
-pins_stepper1 = {A: 18, B: 23, C: 24, D: 25}
-pins_stepper2 = {E: 5, F: 6, G: 13, H: 26}
+# initialize components and wiring
+system = ini.Initialize()
+step_counter_stepper_1 = 0
+step_counter_stepper_2 = 0
+total_sub_steps = len(system.mask_dl)
+# this function could be improved by using threading and running both steppers in parallel
 
-# alternative code for end switches
-stop_x = 27
-stop_y = 17
-# Enable Treiber (I think that is unnecessary) -> enable again if motor fails
-# AA=8
-# BB=7
-#Stop-Button
-STOP = 27
-# 0.3 sec is very slow -> no stepping errors
-# 0.002 sec is possible withlut errors
-s100 = 0.005
-s75 = 0.01
-s50 = 0.025
-s25 = 0.05
-s0  = 0.1
-ticks = 0
-# Pins aus Ausgange definieren
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(A,GPIO.OUT)
-GPIO.setup(B,GPIO.OUT)
-GPIO.setup(C,GPIO.OUT)
-GPIO.setup(D,GPIO.OUT)
-GPIO.setup(AA,GPIO.OUT)
-GPIO.setup(BB,GPIO.OUT)
-GPIO.setup(STOP, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
-GPIO.output(A, False)
-GPIO.output(B, False)
-GPIO.output(C, False)
-GPIO.output(D, False)
-GPIO.output(AA,True)
-GPIO.output(BB,True)
-out_pins = [A,B,C,D]
-#links
-mask_dl =[[1,0,1,0],
-          [1,0,0,1],
-          [0,1,0,1],
-          [0,1,1,0]]
-#rechts
-mask_dr =[[0,1,1,0],
-          [0,1,0,1],
-          [1,0,0,1],
-          [1,0,1,0]]
-gesamt_teil_schritt = len(mask_dl)
-#########################################################
-#global variable
-stop_xA=0
-#########################################################
-def stop_move(STOP):
-    global stop_xA
-    if GPIO.input(STOP)==0:##taster gedrueckt
-        stop_xA=1
-        print("STOP-Taste zu")
-    else: ##taster offen
-        stop_xA=0
-        print("STOP-Taste offen")
 
-GPIO.add_event_detect(STOP, GPIO.BOTH, callback=stop_move, bouncetime=200)
-##########################################################
-def main():
+def move_initial(speed_1, speed_2):
     try:
-        os.system("clear")
-        ticks=100 ## 200: one round
-        move_initial(s25,s0)
-        sleep(1)
-        #move_left(ticks,s25)
-        move_right(ticks,s25)
-        GPIO.cleanup()
-        main_start=True
-        while main_start: ##wird noch nicht genutzt
-            if main_stop==False:
-                main_start=False
-    except:
-        GPIO.cleanup()
-        return False
-
-def move_initial (speed_l,speed_r):
-    global stop_xA
-    try:
-        #1.Teil fahren(links) bis der Taster "gedrückt" stop
-        count_teil_schritt=0
-        while (not stop_xA):
-            for pin in range(0,4): #Elemente in Mask
-                pin_id=out_pins[pin] #Out Zuweisung
-                if mask_dl[count_teil_schritt ][pin]!=0:
-                    #print (" Enable GPIO %i" %(pin_id))
+        # drive first stepper until end switch is pressed.
+        count_sub_steps = 0  # one step consists of four sub-steps
+        while not ini.stop_xA:
+            for pin in range(len(system.mask_dl)):  # elements in mask_dl
+                pin_id = system.out_pins[pin]  # assign out pins
+                if system.mask_dl[count_sub_steps][pin] != 0:
                     GPIO.output(pin_id, True)
                 else:
                     GPIO.output(pin_id, False)
-            count_teil_schritt  = count_teil_schritt  + 1
-            if count_teil_schritt >= gesamt_teil_schritt: #Kompletter Schritt abgeschlossen(4Teilschritte)
-                count_teil_schritt  = 0
-            sleep(speed_l)
+            count_sub_steps += 1
+            if count_sub_steps >= total_sub_steps:  # one step was completed
+                count_sub_steps = 0
+            sleep(speed_1)
         sleep(0.5)
-		#2.Teil fahren bis der Taster wieder "nicht gedrückt" dann 0 Position
-        count_teil_schritt=0
-        while (stop_xA):
-            for pin in range(0,4): #Elemente in Mask
-                pin_id=out_pins[pin] #Out Zuweisung
-                if mask_dr[count_teil_schritt ][pin]!=0:
-                    #print (" Enable GPIO %i" %(pin_id))
+        # drive first stepper until end switch is released.
+        # This position is defined as 0 - position.
+        count_sub_steps = 0
+        while ini.stop_xA:
+            for pin in range(len(system.mask_dr)):  # elements in mask_dr
+                pin_id = system.out_pins[pin]  # assign out pins
+                if system.mask_dr[count_sub_steps][pin] != 0:
                     GPIO.output(pin_id, True)
                 else:
                     GPIO.output(pin_id, False)
-            count_teil_schritt  = count_teil_schritt  + 1
-            if count_teil_schritt >= gesamt_teil_schritt: #Kompletter Schritt abgeschlossen(4Teilschritte)
-                count_teil_schritt  = 0
-            sleep(speed_r)
-    except:
+            count_sub_steps += 1
+            if count_sub_steps >= total_sub_steps:  # one step was completed
+                count_sub_steps = 0
+            sleep(speed_2)
+    except KeyboardInterrupt:
         GPIO.cleanup()
         print("x")
         return False
 
-def move_left (ticks,speed):
-	# Der Winkel enspricht 1,8Grad d.h dass bei 200 gesamtschritten(ist 1 seqenzdurchlauf) 360Grad erreicht sind
-    try:
-        count_teil_schritt=0
-        for i in range(0,ticks):
-            for pin in range(0,4): #Elemente in Mask
-                pin_id=out_pins[pin] #Out Zuweisung
-                if mask_dl[count_teil_schritt ][pin]!=0:
-                    #print (" Enable GPIO %i" %(pin_id))
-                    GPIO.output(pin_id, True)
-                else:
-                    GPIO.output(pin_id, False)
-            count_teil_schritt  = count_teil_schritt  + 1
-            if count_teil_schritt >= gesamt_teil_schritt: #Kompletter Schritt abgeschlossen(4Teilschritte)
-                count_teil_schritt  = 0
-            sleep(speed)
-    except:
-        GPIO.cleanup()
-        return False
 
-def move_right (ticks,speed):
-	# Der Winkel enspricht 1,8Grad d.h dass bei 200 gesamtschritten(ist 1 seqenzdurchlauf) 360Grad erreicht sind
-    try:
-        count_teil_schritt=0
-        for i in range(0,ticks):
-            for pin in range(0,4): #Elemente in Mask
-                pin_id=out_pins[pin] #Out Zuweisung
-                if mask_dr[count_teil_schritt ][pin]!=0:
-                    #print (" Enable GPIO %i" %(pin_id))
-                    GPIO.output(pin_id, True)
-                else:
-                    GPIO.output(pin_id, False)
-            count_teil_schritt  = count_teil_schritt  + 1
-            if count_teil_schritt >= gesamt_teil_schritt: #Kompletter Schritt abgeschlossen(4Teilschritte)
-                count_teil_schritt  = 0
-            sleep(speed)
-    except:
-        GPIO.cleanup()
-        return False
-###########################################################################
-if __name__ == '__main__':
-      main()
-sys.exit(0)
+move_initial(system.speeds["s25"], system.speeds["s0"])
+GPIO.cleanup()
+#todo: ticks = 0
+
